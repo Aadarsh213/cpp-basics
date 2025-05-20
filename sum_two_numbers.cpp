6e64c2ffa9c370c58e78f77270cf7bb39efbd44c
Aadarsh Kumar <aadarshkr956058@gmail.com>
2025-05-20 12:31:14 +0530

    Add files via upload

---
+++ sum_two_numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Enter First number:"<<endl;
+int a;
+cin>>a;
+cout<<"enter second number:"<<endl;
+int b;
+cin>>b;
+cout<<"sum is:"<<endl;
+cout<<a+b<<endl;
+
+return 0;
+
+
+
+}
