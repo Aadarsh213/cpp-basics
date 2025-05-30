e0e11d6f76d88cec00008cfd2a81c7288c84eb08
Aadarsh Kumar <aadarshkr956058@gmail.com>
2025-05-30 22:40:31 +0530

    Add files via upload

---
+++ prime_check.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+      int n;
+      cout<<"enter number to check";
+      cin>>n;
+      if(n%2==0){
+          cout<<"Even";
+      }
+      else 
+      {
+          cout<<"Odd";
+      }
+      return 0;
+
+}
