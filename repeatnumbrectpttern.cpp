2508d5f11e93fbd5f5c3566e02411499d3ec897d
Aadarsh Kumar <aadarshkr956058@gmail.com>
2025-06-06 23:07:10 +0530

    Add files via upload

---
+++ repeatnumbrectpttern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int m, n;
+cout<<"Enter length of rectangle: ";
+cin>>m;
+cout<<"Enter breadth of rectangle: ";
+cin>>n;
+for(int i=1;i<=m; i++){
+    for(int j=1; j<=n; j++){
+        cout<<i;
+    }
+      cout<<endl;
+}
+
+}
